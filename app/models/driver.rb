022c93e420463e168e7ff427b7fd01dd7a156eef
Joshua Sebastian <jbs87@hotmail.com>
2014-12-09 17:24:47 -0500

    Edited driver models

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,5 +1,11 @@
 class Driver < ActiveRecord::Base
-  has_many :player_teams #, foreign_key: 'driver1_id'
-  has_many :player_teams #, foreign_key: 'driver2_id'
+
+  has_many :player_teams_as_primary, class_name: "PlayerTeam", source: :primary_driver
+  has_many :player_teams_as_secondary, class_name: "PlayerTeam", source: :secondary_driver
   has_many :team_members
+
+  def player_teams
+  	PlayerTeam.where("driver1_id = ? or driver2_id = ?", self.id, self.id)
+  end
+
 end
