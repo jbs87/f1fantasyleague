0af2ae64425b14c3e0837dd9469e9744109518a8
Rudi Luzar <rudi@rudicode.com>
2014-12-11 17:49:53 -0500

    Extracted first_race seed into its own class.

--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Gpmanager
   class Application < Rails::Application
+    # config.autoload_paths << Rails.root.join('lib')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
@@ -19,5 +19,6 @@ module Gpmanager
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
   end
 end
