9384dbea4c2bc99d9ad2a46a99983b66e1bb26f5
Joshua Sebastian <jbs87@hotmail.com>
2014-12-11 13:37:20 -0500

    Updated gem file

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -1,7 +1,35 @@
 class TeamMember < ActiveRecord::Base
-  has_many :race_results
-  belongs_to :driver
-  belongs_to :engine
-  belongs_to :chassis_manufacturer
-  belongs_to :race
+	attr_accessor :name, :round
+
+	include HTTParty
+	base_uri 'http://ergast.com/api/'
+
+	has_many :race_results
+	belongs_to :driver
+	belongs_to :engine
+	belongs_to :chassis_manufacturer
+	belongs_to :race
+
+	def initialize(name, round)
+      @name   = name.capitalize
+      @round  = round
+    end
+
+	def self.tmembers
+		racers = {}
+		race_data.each do |racer|
+			#racers.push(racer["Driver"]["driverId"])
+			racers[:racer["Driver"]["driverId"]] = racer["Constructor"]["constructorId"]
+		end
+		binding.pry
+	end
+
+	 private
+    
+      def self.race_data
+        response = get("/f1/2014/1/results.json")
+		race = response['MRData']['RaceTable']['Races'][0]["Results"]
+      end
+
+
 end
