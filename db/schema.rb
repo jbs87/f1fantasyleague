85a0a91045e73974d1977f0bf34132d33260c168
Rudi Luzar <rudi@rudicode.com>
2014-12-12 14:41:37 -0500

    backed up the database and outputed some tables to .sql

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20141211220313) do
 
   create_table "chassis_manufacturers", force: true do |t|
