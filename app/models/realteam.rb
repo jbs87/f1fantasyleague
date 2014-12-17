c8df003ced4a17cffed336e84e8d0909b2c2adf4
Joshua Sebastian <jbs87@hotmail.com>
2014-12-17 12:51:11 -0500

    Added Realteam model and method for validation

--- app/models/realteam.rb
+++ app/models/realteam.rb
@@ -1,2 +1,24 @@
 class Realteam < ActiveRecord::Base
+
+	def self.isValidTeam(pt)
+		if self.find_by(driver1_name: pt.primary_driver.name, driver2_name: pt.secondary_driver.name)
+			return false
+		end
+		if self.find_by(engine_name: pt.engine.name, chassis_name: pt.chassis_manufacturer.name)
+			return false
+		end
+		if self.find_by(driver1_name: pt.primary_driver.name, chassis_name: pt.chassis_manufacturer.name)
+			return false
+		end
+		if self.find_by(driver1_name: pt.primary_driver.name, engine_name: pt.engine.name)
+			return false
+		end
+		if self.find_by(driver2_name: pt.secondary_driver.name, chassis_name: pt.chassis_manufacturer.name)
+			return false
+		end
+		if self.find_by(driver2_name: pt.secondary_driver.name, engine_name: pt.engine.name)
+			return false
+		end
+		return true
+	end
 end
