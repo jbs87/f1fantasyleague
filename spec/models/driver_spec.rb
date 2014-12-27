68c2b4767d8bff3b98eb89754f2846260dc0876b
Rudi Luzar <rudi@rudicode.com>
2014-12-27 00:10:08 -0500

    Updated driver and user specs, because test db is now using seeds.rb

--- spec/models/driver_spec.rb
+++ spec/models/driver_spec.rb
@@ -3,18 +3,17 @@ require 'spec_helper'
 describe Driver do
 	before :each do
     # @user = User.new "Jack", "jack@temp.com", :category
-    @driver =  Driver.new({ name: 'Nico Rosberg', price: 50000000, driverId: 'rosberg'})
-    @track = Track.new({circuitId:'australia'})
-	@race = Race.new({ date: DateTime.new(2014,03,16,8,0,0), track_id: , password: 'b', password_confirmation: 'b'})
+    # @driver =  Driver.new({ name: 'Nico Rosberg', price: 50000000, driverId: 'rosberg'})
+    # @track = Track.new({circuitId:'australia'})
+	  # @race = Race.new({ date: DateTime.new(2014,03,16,8,0,0), track_id: , password: 'b', password_confirmation: 'b'})
 	end
 
   describe "#index" do
-    
   end
 
 	describe "#new" do
-		it "takes parameters and returns a User object" do
-			expect(@user).to be_an_instance_of User
+		xit "takes parameters and returns a User object" do
+			# expect(@user).to be_an_instance_of User
 		end
 	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,10 @@ require "pry"
 describe User do
 	it "should authenticate with matching username and password" do
 		user = create(:user, password_confirmation: "app")
-		race = create(:race)
-		User.authenticate("a@goop.com","app").should == user 
+		# race = create(:race)
+    # User.authenticate("a@goop.com","app").should == user 
+    result = User.authenticate("a@goop.com","app")
+		expect(result).to eq(user) 
 	end
 end
 
